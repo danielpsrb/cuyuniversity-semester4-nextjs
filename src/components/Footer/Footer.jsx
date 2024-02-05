a493fed47d7d2dad45f3a0ebef7401456fbabf1d
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-05 20:48:26 +0700

    last commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div>
             <footer className="bg-color-black flex justify-center items-center p-4 text-color-aqua text-base-conten border-y-2 border-color-aqua">
                 <aside>
-                    <p>Copyright © 2023 | All right reserved by Dans</p>
+                    <p>Copyright © 2024 | All right reserved by Dans</p>
                 </aside>
             </footer>
         </div>
