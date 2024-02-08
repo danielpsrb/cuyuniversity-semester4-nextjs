a41e7ec37ec18e207be8caa56cb1026d72ed2269
danielpsrb <danielpsrb01@gmail.com>
2024-02-08 11:40:09 +0700

    add title icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,11 @@ const gabarito = Gabarito({ subsets: ['latin'] })
 export const metadata = {
   title: 'Dans Animelist',
   description: 'Website Anime Indonesia',
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['/apple-touch-icon.png']
+  }
 }
 
 export default function RootLayout({ children }) {
