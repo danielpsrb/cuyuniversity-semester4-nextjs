3988e94793f848f1f756534cd7ab7489f720acc0
danielpsrb <danielpsrb01@gmail.com>
2024-02-09 00:27:44 +0700

    change nav title

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,11 @@
-import Link from "next/link"
 import InputSearch from "./InputSearch"
 import UserActionButton from "./UserActionButton"
-import Image from "next/image"
 
 const Navbar = () => {
     return (
         <header className="bg-color-primary">
             <div className="flex md:flex-row flex-col justify-between md:items-center p-4 gap-2">
-                <h2 className="font-bold md:text-2xl text-xl text-color-black mx-auto md:mx-0 select-none">✨DANS_ANIMELIST✨</h2>
+                <h2 className="font-bold md:text-2xl text-xl text-color-black mx-auto md:mx-0 select-none">D-ANIMELIST 2.0 🔥</h2>
                 <InputSearch/>
                 <UserActionButton/>
             </div>
