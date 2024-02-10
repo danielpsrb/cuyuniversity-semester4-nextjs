20b0adb5f84d6145a6f022fc0b819185e3088fcf
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-10 12:05:12 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,13 +4,20 @@ import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-
 import Footer from "@/components/Footer/Footer";
 import { authUserSession } from "@/libs/auth-libs";
 import LoginSuccessMessage from "@/components/Dashboard/LoginSucessMessage";
+import { saveUserData } from "./users/saveUsers/page";
 
 const Page = async() => {
   // const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
   // const topAnime = await response.json()
 
   const user = await authUserSession();
-
+  if(user) {
+    await saveUserData({
+        name: user.name,
+        email: user.email,
+        image: user.image,
+    })
+  }
 
   const topAnime = await getAnimeResponse("top/anime", "limit=8")
   let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
