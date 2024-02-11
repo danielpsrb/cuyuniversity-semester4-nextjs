39635fad7def088b3c1689551e5dbe2138e88119
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-11 23:38:52 +0700

    Update api-libs.js

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -4,20 +4,9 @@ export const getAnimeResponse = async(resource, query) => {
     return anime
 }
 
-// export const getNestedAnimeResponse = async (resource, objectProperty) => {
-//     const response = await getAnimeResponse(resource);
-//     // console.log("Response:", response); // Add this logging statement
-//     if (response && response.data) {
-//         // console.log("Response data:", response.data); // Add this logging statement
-//         return response.data.flatMap(item => item[objectProperty]);
-//     } else {
-//         console.error("Response or response data is undefined");
-//         return []; // Return an empty array or handle the error as appropriate
-//     }
-// };
-
 export const getNestedAnimeResponse = async(resource, objectProperty) => {
     const response = await getAnimeResponse(resource)
+    // return response?.data?.flatMap(item => item[objectProperty]) || [];
     return response.data.flatMap(item => item[objectProperty])
 }
 
