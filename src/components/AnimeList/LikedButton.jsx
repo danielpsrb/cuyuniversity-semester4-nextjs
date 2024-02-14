0364b45e197c9c5ba1fd423dc5b280efa16faa5d
danielpsrb <danielpsrb01@gmail.com>
2024-02-15 02:37:55 +0700

    add new

---
+++ src/components/AnimeList/LikedButton.jsx
@@ -0,0 +1,47 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import Swal from "sweetalert2";
+
+
+const LikedButton = ({ commentId }) => {
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+
+  // useEffect(() => {
+  //   const fetchLikesCount = async () => {
+  //     try {
+  //       const response = await fetch(`/api/v1/likes/${commentId}`);
+  //       const data = await response.json();
+  //       setLikesCount(data.likes);
+  //     } catch (error) {
+  //       console.error('Error fetching likes:', error);
+  //     }
+  //   };
+  //   fetchLikesCount();
+  // }, [commentId]);
+
+  const handleLiked = (e) => {
+    e.preventDefault();
+    Swal.fire({
+      text: "Sorry, Liked Comment feature is not Currently Available, because it is still in the development stage",
+      icon: "error",
+      confirmButtonColor: "#3085d6",
+    })
+    return setLiked(!liked);
+    // You can also implement logic here to send a request to like/unlike the comment
+  }
+
+  return (
+    <>
+      <div className="absolute bottom-0 right-0 mb-2 mr-2">
+        <button onClick={handleLiked}>
+          {liked ? <AiFillHeart className='text-color-red' size={20} /> : <AiOutlineHeart className='text-color-primary' size={20} />}
+        </button>
+        <span>{likesCount}</span>
+      </div>
+    </>
+  )
+}
+
+export default LikedButton;
