f6bd6ecb78d869b1d2be08a163474fe504c1a0ba
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-15 10:18:58 +0700

    last commit route

--- src/app/api/v1/comment/route.js
+++ src/app/api/v1/comment/route.js
@@ -3,8 +3,17 @@ import getComments from '@/app/getComment/page';
 
 export async function POST(request) {
   try {
-      const { anime_mal_id, user_email, comment, username, anime_title, userId, rating, date } = await request.json();
+      const { anime_mal_id, user_email, comment, username, anime_title, rating, date } = await request.json();
 
+      // const user = await prisma.user.findFirst({
+      //     where: {
+      //         email: user.email
+      //     }
+      // })
+      // if (!user) {
+      //   return Response.json({ status: 404, error: "Account not found" });
+      // }
+      
       const user = await prisma.user.findUnique({
         where: {
           email: user_email
@@ -18,6 +27,9 @@ export async function POST(request) {
         return Response.json({ status: 404, error: "User not found" });
       }
 
+
+      // const userId = user.id;
+
       const createComment = await prisma.comment.create({
           data: {
               anime_mal_id,
@@ -27,28 +39,61 @@ export async function POST(request) {
               anime_title,
               rating,
               date,
-              userId // Menggunakan userId yang telah didapatkan
+              userId: user.id 
           },
       });
       if(createComment) return Response.json({ status: 201, isCreated: true });
-      else return Response.json({ status: 500, error: "Internal Server Error" });
   } catch(error) {
       console.error("Error creating comment:", error);
+      return Response.json({ status: 500, error: "Internal Server Error" });
   }
-}
+}import prisma from '@/libs/prisma';
+import getComments from '@/app/getComment/page';
 
-export async function GET(request) {
+export async function POST(request) {
   try {
-    const { searchParams } = new URL(request.url)
-    const query = searchParams.get('anime_mal_id');
-    if (!query) {
-      // Jika anime_mal_id tidak ada, kembalikan 400 Bad Request
-      return Response.json({ status: 400, error: "Bad Request: Ups Sorry Your Anime mal id is empty" });
-    }
-    const comments = await getComments(query);
-    return Response.json({ comments });
-  } catch (error) {
-    console.error("Error retrieving comments:", error);
-    return Response.json({ status: 500, error: "Internal Server Error" });
+      const { anime_mal_id, user_email, comment, username, anime_title, rating, date } = await request.json();
+
+      // const user = await prisma.user.findFirst({
+      //     where: {
+      //         email: user.email
+      //     }
+      // })
+      // if (!user) {
+      //   return Response.json({ status: 404, error: "Account not found" });
+      // }
+      
+      const user = await prisma.user.findUnique({
+        where: {
+          email: user_email
+        },
+        select: {
+          id: true
+        }
+      });
+  
+      if (!user) {
+        return Response.json({ status: 404, error: "User not found" });
+      }
+
+
+      // const userId = user.id;
+
+      const createComment = await prisma.comment.create({
+          data: {
+              anime_mal_id,
+              user_email,
+              comment,
+              username,
+              anime_title,
+              rating,
+              date,
+              userId: user.id 
+          },
+      });
+      if(createComment) return Response.json({ status: 201, isCreated: true });
+  } catch(error) {
+      console.error("Error creating comment:", error);
+      return Response.json({ status: 500, error: "Internal Server Error" });
   }
 }
