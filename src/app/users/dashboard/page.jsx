e94ba3c1bb62c1fe3fdfb619a040057f73f68b88
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-25 20:49:00 +0700

    update dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,4 +1,4 @@
-import { authUserSession } from "@/services/auth-services";
+import { authUserSession } from "@/libs/auth-libs"
 import Image from "next/image"
 import Link from "next/link"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
