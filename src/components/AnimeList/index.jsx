be4c3ceaee7115561c585d961b9280d6fcf6baea
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-25 12:10:41 +0700

    updated

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const AnimeList = ({ api }) => {
     return (
-        <div className="grid md:grid-cols-4  grid-cols-2 gap-2 px-4">
+        <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-5 px-4">
             {api.data?.map((anime, index) => {
                 return (
                     <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-color-primary hover:text-color-aqua transition-all" key={index}>
