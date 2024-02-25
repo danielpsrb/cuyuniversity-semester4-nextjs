f1230530c2dcf43598f82a97dc32a474390478fa
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-02-25 14:05:01 +0700

    last commits

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -9,10 +9,11 @@ const AnimeList = ({ api }) => {
                     <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-color-primary hover:text-color-aqua transition-all" key={index}>
                         <Image 
                             src={anime.images.webp.image_url} 
-                            alt="..." 
+                            alt="animeImages" 
                             width={350} 
                             height={350} 
                             className="w-full max-h-80 object-cover" 
+                            priority={true}
                         />
                         <h3 className="font-bold md:text-xl text-md p-4">{anime.title}</h3>
                     </Link>
