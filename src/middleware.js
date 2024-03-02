2fa3d6cfcaec832c1f59afa33a1a8a0b53fa5a38
danielpsrb <danielpsrb01@gmail.com>
2024-03-03 00:31:48 +0700

    updated middleware for nextauth-v5

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,21 @@
-export { default } from "next-auth/middleware"
+import { NextResponse, NextRequest } from "next/server";
+import NextAuth from "next-auth";
+import authConfig from "../auth.config";
 
-export const config = { matcher: ["/users/:path*"] }
+const { auth } = NextAuth(authConfig);
+
+export async function middleware(request) {
+
+  const session = await auth()
+
+  const { pathname } = request.nextUrl;
+
+  if (!session && pathname.startsWith('/users')) {
+    // Redirect to the unauthorized page
+    return NextResponse.redirect(new URL("/unAuthorized", request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
