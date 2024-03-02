968d548f54d87a2d32f7d4874c12aeeaecbdc6cc
danielpsrb <danielpsrb01@gmail.com>
2024-03-03 00:28:10 +0700

    updated auth-config

--- auth.config.js
+++ auth.config.js
@@ -1,5 +1,8 @@
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import Gitlab from "next-auth/providers/gitlab";
+import Discord from "next-auth/providers/discord";
+
 
 export default {
     providers: [
@@ -10,6 +13,14 @@ export default {
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+        }),
+        Gitlab({
+            clientId: process.env.GITLAB_CLIENT_ID,
+            clientSecret: process.env.GITLAB_CLIENT_SECRET
+        }),
+        Discord({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET
+        }),
     ]
 }
