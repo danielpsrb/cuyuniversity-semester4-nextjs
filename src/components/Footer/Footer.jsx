630aef2b0cf63979ff388d923a871d5884357ed3
danielpsrb <danielpsrb01@gmail.com>
2024-05-07 04:54:13 +0700

    fix: update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="bg-color-black flex justify-center items-center p-4 text-color-aqua text-base-conten border-y-2 border-color-aqua">
+            <footer className="bg-color-darkgray text-color-black dark:bg-color-black flex justify-center items-center p-4 dark:text-color-aqua text-base-conten border-y-2 dark:border-color-aqua">
                 <aside>
                     <p>Copyright © {new Date().getFullYear()} | All right reserved by Dans</p>
                 </aside>
